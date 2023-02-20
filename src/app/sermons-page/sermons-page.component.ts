27c7fdad88699a1dfe0f73ef11b6eae0b4d578da
Colin -FROST <colinwiesen@gmail.com>
2023-02-20 10:21:41 -0800

    touch-up on /about and /sermons

--- src/app/sermons-page/sermons-page.component.ts
+++ src/app/sermons-page/sermons-page.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class SermonsPageComponent {
 
+  sermonList:any[][] = [["The Armor Of God","Eph 6:11", "About the bible"],
+                        ["The Armor Of God Pt2","Eph 6:11", "About the bible"],
+                        ["The Armor Of God Pt3","Eph 6:12", "About the bible"],
+                        ["The Armor Of God Pt4","Eph 6:13", "About the bible"],
+                        ["The Armor Of God Pt5","Eph 6:14", "About the bible"],
+                        ["The Armor Of God","Eph 6:11", "About the bible"],
+                        ["The Armor Of God Pt2","Eph 6:11", "About the bible"],
+                        ["The Armor Of God Pt3","Eph 6:12", "About the bible"],
+                        ["The Armor Of God Pt4","Eph 6:13", "About the bible"],
+                        ["The Armor Of God Pt5","Eph 6:14", "About the bible"],
+                      ]
+
 }
